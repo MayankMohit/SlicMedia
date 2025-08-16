d12823374ac8739a3e923a0bc2466aff4002381a
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2025-08-16 18:29:10 +0530

    Dark bg added

--- frontend/src/components/buttons/neuButton.jsx
+++ frontend/src/components/buttons/neuButton.jsx
@@ -1,6 +1,6 @@
 const NeuButton = ({text}) => {
   return (
-      <button className="px-4 py-1 mt-1 font-medium rounded-sm bg-[#c3c3c3] text-black dark:bg-green-400 dark:text-black w-fit transition-all shadow-[3px_-3px_0px_black] dark:shadow-[3px_-3px_0px_gray] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[-3px]">
+      <button className="px-4 py-1 mt-1 font-medium rounded-sm bg-gray-200 text-black dark:bg-gray-500 dark:text-gray-100 w-fit transition-all shadow-[3px_-3px_0px_black] dark:shadow-[3px_-3px_0px_white] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[-3px]">
         {text}
       </button>
   );
--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -1,4 +1,5 @@
-import bgnight from '../assets/noiseLight2.png';
+import bgDay from '../assets/noiseLight.png';
+import bgnight from '../assets/noiseDark.png';
 import { useTheme } from '../hooks/common/useTheme.js';
 import { ArrowDown } from 'lucide-react';
 
@@ -9,23 +10,23 @@ const Hero = () => {
     <div
       className="absolute top-0 left-0 w-full h-screen bg-neutral-300 dark:bg-zinc-900"
       style={{
-        backgroundImage: `url(${bgnight})`,
+        backgroundImage: `url(${darkMode ? bgnight : bgDay})`,
         backgroundRepeat: 'repeat',
         backgroundSize: 'auto',
       }}
     >
       <div className="flex items-center justify-start h-full">
         <div className="ml-15 mt-30">
-          <div className="text-2xl font-bold text-gray-500 dark:text-gray-200 mb-10 ml-2 select-none">  
+          <div className="text-2xl font-bold text-gray-500 dark:text-gray-400 mb-10 ml-2 select-none">  
             B2B Service Agency
           </div>
-          <p className="mt-4 text-[100px] w-[50rem] text-black font-bold font-sans leading-[6.3rem] tracking-[-4.8px] dark:text-gray-400 select-none">
+          <p className="mt-4 text-[100px] w-[50rem] text-black font-bold font-sans leading-[6.3rem] tracking-[-4.8px] dark:text-gray-200 select-none">
             Designing <span className='cedarville-cursive-regular'>Visuals</span> that Elevate your Brand.
           </p>
-          <p className="mt-15 text-2xl w-[30rem] text-gray-500 font-bold dark:text-gray-200 ml-2 select-none leading-[1.6rem]">
+          <p className="mt-15 text-2xl w-[30rem] text-gray-500 font-bold dark:text-gray-400 ml-2 select-none leading-[1.6rem]">
             At SLIC Media, we help DTC brands scale their online presence with high-converting video creatives, content strategies, and growth packages tailored for results.
           </p>
-          <p className="mt-3 text-xl text-bold text-gray-600 dark:text-gray-400 mb-10 ml-2  select-none">
+          <p className="mt-3 text-xl text-bold text-gray-600 dark:text-gray-400 mb-20 ml-2  select-none">
             Discover More 
             <button className='ml-3 px-2 py-1 bg-[#c60867] text-white rounded-md '>
               <ArrowDown className="inline-block" />
